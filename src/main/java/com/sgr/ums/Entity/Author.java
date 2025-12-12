66a459f4c2a39ff0f661d45534d59985512ab35c
sagargoud241 <sagargoud241@gmail.com>
2025-12-12 01:59:28 +0000

    author entity

---
+++ src/main/java/com/sgr/ums/Entity/Author.java
@@ -0,0 +1,36 @@
+package com.sgr.ums.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="author")
+public class Author extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="Id")
+    private  long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "full_name")
+    private String fullName;   // auto hunu parxa , combination of firstName  middleName  lastName (add update grda
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "address", nullable = false)
+    private String  address;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+}
